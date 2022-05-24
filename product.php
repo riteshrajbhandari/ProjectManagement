46872fe4091b1fff8b06aaaed9ac142c0df11f10
Sajid_Miya <sajidmiya038@gmail.com>
2022-05-24 13:33:03 +0545

    border-ma-kaam -vax

--- product.php
+++ product.php
@@ -137,7 +137,7 @@ session_start();
                 <div class="row product ">
                     <div class="col-lg-6">  
                         <div class="w-100 py-3" style="">
-                            <img src="<?php echo $img_url; ?>" alt="" srcset="" style="width: 400px; height: 350px;box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;">
+                            <img src="<?php echo $img_url; ?>" alt="" srcset="" style="width: 400px; height: 350px;box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;border-radius:1em;">
                         </div>
                         
                     </div>
